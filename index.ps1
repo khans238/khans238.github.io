1e91a24a942a3a13344c2016e3247494bde0ddb9
khans238 <khans238@mcmaster.ca>
2025-03-03 19:14:21 -0500

    assignment 1

---
+++ index.ps1
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!--
+SA: I Sarah Khan, 400247943 certify that this material is my original work. 
+No other person's work has been used without due acknowledgement.
+I have also not made my work available to anyone else -->
+<head>
+    <title> [Sarah Khan] [Lab 1] </title>
+</head>
+<body>
+    <style>
+        body {font-family: Arial}
+        h1 {font-family: 'Times New Roman', Times, serif}
+        h2 {font-family: 'Times New Roman', Times, serif}
+    </style>
+    <img src="ResumeIMG.jpg" width = "150" height= "170" Sarah Khan /> &nbsp;&nbsp;&nbsp;&nbsp;  <h1> <i> Sarah Khan </i> </h1>
+
+    <h2> Education </h2>
+    <p><b>Glenview Park Secondary School<br/></b>
+        Cambridge, Ontario<br/></p>
+          <p><b>McMaster University Hamilton, ON</b><br/>
+          Hamilton, Ontario<br/>
+          Automation Engineering Technology, Certificate in Management<br/>
+        </p>
+
+        <h2> Work </h2>
+        <p><b><i> Deloitte </i></b><br/>
+            &nbsp; Working as a Senior Analyst with clients in the Telecommunications, Media and Technology space to help them obtaion funding for their 
+            R&D projects <br/> &nbsp; Interviewing and consulting, presenting to stakeholders to enhance company efficiency as part of the Global investments and Innovation Incentives team <br/>
+            &nbsp; Writing technical reports helping global clients claim several million dollars in funding and expenses <br/> &nbsp;
+        <a href = "https://www2.deloitte.com/ca/en/pages/tax/solutions/global-investment-and-innovation-incentives.html" >Visit Deloitte (Gi3) Website</a>
+        <p><b><i> Apotex </i></b><br/>
+            &nbsp;Worked an 8 month under manufacturing and line engineers <br/> &nbsp;Used VBA to streamline SAP preventative maintenance tasks <br/>
+            &nbsp;Helped design bambevi bottles for new production line <br/>
+    <a href = "https://www.apotex.com/ca/en/" >Visit Apotex Website</a>
+    <p><b><i> Stem Camp </i></b><br/>
+        &nbsp; Lead teams of middle school children through coding excercises using microcontrollers <br/>  &nbsp; Developed creative activities to integrate science and computing with arts and crafts to engage children <br/>
+        &nbsp; Managed team of 10 employees and developed schedules <br/>
+<a href = "https://stemcamp.ca/ontario-camp-locations/summer-camp-dundas/" >Visit Stem Camp Website</a>
+</p>
+
+<h2>Hobbies</h2>
+<p><b><i>a.Hobbies</i></b><br/>
+Crocheting:I consider skating my favourite hobby because I have been doing it since the age of 8 and I enjoy anything I can do with my hands as well as creating stuff. It allows me to be creative and make cute things. <br/></p>
+<p></p><b><i>b.Interests</i></b><br/>
+Reading: I enjoy reading in my downtime. It is a nice away to get away from looking at screens all the time. <br/>
+<p></p><b><i>c.Professional Associations</i></b><br/>
+Women in Engineering Society: I served as a peer mentor here. It is a great way to connect with other women in the industry. <br/>
+<p></p><b><i>d.Clubs</i></b><br/>
+BTA: worked on the bachelor of technoogy association as a social media coordinator. I helped curate and design social media posts across a variety of platforms.
+<p> <footer> Copyright 2025 Sarah Khan  </footer></p>
+
+</body>
+</html>
